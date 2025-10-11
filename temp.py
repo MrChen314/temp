2eae7acca55db9641164038b9288884d97ce37b4
Chen Quan Lin <114123769+MrChen314@users.noreply.github.com>
2025-10-11 17:04:11 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,10 @@
-The expanded size of the tensor (2048) must match the existing size (768) at non-singleton dimension 1.  Target sizes: [768, 2048].  Tensor sizes: [2048, 768]
+Cannot copy out of meta tensor; no data!
   File "/home/c30061641/Veomni/VeOmni_Qwen3Moe/veomni/models/transformers/qwen3_moe/modeling_qwen3_moe.py", line 391, in post_init
-    fused_experts.gate_proj.data[expert_idx] = expert.gate_proj.weight.data.T.clone()
+    fused_experts.gate_proj.data[expert_idx] = expert.gate_proj.weight.data.clone()
   File "/home/c30061641/Veomni/VeOmni_Qwen3Moe/veomni/models/auto.py", line 130, in build_foundation_model
     module.post_init()
   File "/home/c30061641/Veomni/VeOmni_Qwen3Moe/tasks/train_torch.py", line 152, in main
     model = build_foundation_model(
   File "/home/c30061641/Veomni/VeOmni_Qwen3Moe/tasks/train_torch.py", line 405, in <module>
     main()
-RuntimeError: The expanded size of the tensor (2048) must match the existing size (768) at non-singleton dimension 1.  Target sizes: [768, 2048].  Tensor sizes: [2048, 768]
+NotImplementedError: Cannot copy out of meta tensor; no data!
