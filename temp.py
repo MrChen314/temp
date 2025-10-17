664c40b5140bc4228c0bf1539707bd081839b96b
Chen Quan Lin <114123769+MrChen314@users.noreply.github.com>
2025-10-17 18:18:43 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,33 +1,190 @@
-    def set_modules_to_forward_prefetch(model, num_to_forward_prefetch):
-        for i, layer in enumerate(model):
-            if i >= len(model) - num_to_forward_prefetch:
-                break
-            layers_to_prefetch = [
-                model[i + j] for j in range(1, num_to_forward_prefetch + 1)
-            ]
-            layer.set_modules_to_forward_prefetch(layers_to_prefetch)
-    
-    def set_modules_to_backward_prefetch(model, num_to_backward_prefetch):
-        for i, layer in enumerate(model):
-            if i < num_to_backward_prefetch:
-                continue
-            layers_to_prefetch = [
-                model[i - j] for j in range(1, num_to_backward_prefetch + 1)
-            ]
-            layer.set_modules_to_backward_prefetch(layers_to_prefetch)
+class Qwen2_5_VLVisionSdpaAttention(nn.Module):
+    def __init__(self, dim: int, num_heads: int = 16) -> None:
+        super().__init__()
+        self.num_heads = num_heads
+        self.qkv = nn.Linear(dim, dim * 3, bias=True)
+        self.proj = nn.Linear(dim, dim)
 
-    # todo:vit第一层不切，但挂hook prefetch后面层；切llm的embedding和llm_head
-    set_modules_to_forward_prefetch(model.visual.blocks, num_to_forward_prefetch=2)
-    model.visual.blocks[-1].set_modules_to_forward_prefetch([model.model.layers[0]])
-    set_modules_to_forward_prefetch(model.model.layers, num_to_forward_prefetch=1)
-    set_modules_to_backward_prefetch(model.visual.blocks, num_to_backward_prefetch=2)
-    set_modules_to_backward_prefetch(model.model.layers, num_to_backward_prefetch=1)
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        cu_seqlens: torch.Tensor,
+        rotary_pos_emb: Optional[torch.Tensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+    ) -> torch.Tensor:
+        seq_length = hidden_states.shape[0]
+        
+        # ulysses sp patch: qkv projection
+        qkv = self.qkv(hidden_states).reshape(seq_length, 3, self.num_heads, -1).permute(1, 0, 2, 3)
+        
+        unpadded_dim_size = cu_seqlens[-1]
+        if get_parallel_state().ulysses_enabled:
+            qkv = gather_seq_scatter_heads(qkv, seq_dim=1, head_dim=2)
+            sp_padding_size = qkv.size(1) - unpadded_dim_size
+            if sp_padding_size > 0:
+                qkv = unpad_tensor(qkv, dim=1, padding_size=sp_padding_size)
+            seq_length = qkv.shape[1]
+        q, k, v = qkv.unbind(0)
+
+        if position_embeddings is None:
+            logger.warning_once(
+                "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
+                "through `rotary_pos_emb` (2D tensor of RoPE theta values), to using externally computed "
+                "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.54 `rotary_pos_emb` will be "
+                "removed and `position_embeddings` will be mandatory."
+            )
+            emb = torch.cat((rotary_pos_emb, rotary_pos_emb), dim=-1)
+            cos = emb.cos()
+            sin = emb.sin()
+        else:
+            cos, sin = position_embeddings
+        q, k = apply_rotary_pos_emb_vision(q, k, cos, sin)
+
+        attn_output = torch_npu.npu_fusion_attention(
+                q, k, v, q.shape[1],
+                pse=None,
+                padding_mask=None,
+                atten_mask=None,
+                scale=1.0 / math.sqrt(q.shape[-1]),
+                keep_prob=1,
+                input_layout='TND',
+                actual_seq_qlen=cu_seqlens.tolist()[1:],
+                actual_seq_kvlen=cu_seqlens.tolist()[1:],
+                pre_tockens=2147483647,
+                next_tockens=2147483647,
+                sparse_mode=0)[0]
+
+        # attention_mask = torch.zeros([1, seq_length, seq_length], device=q.device, dtype=torch.bool)
+        # for i in range(1, len(cu_seqlens)):
+        #     attention_mask[..., cu_seqlens[i - 1] : cu_seqlens[i], cu_seqlens[i - 1] : cu_seqlens[i]] = True
+        # q = q.transpose(0, 1)
+        # k = k.transpose(0, 1)
+        # v = v.transpose(0, 1)
+        # attn_output = F.scaled_dot_product_attention(
+        #     q.unsqueeze(0), k.unsqueeze(0), v.unsqueeze(0), attention_mask, dropout_p=0.0
+        # )
+        # attn_output = attn_output.squeeze(0).transpose(0, 1)
+        if get_parallel_state().ulysses_enabled:
+            attn_output = pad_tensor(attn_output, dim=0, padding_size=sp_padding_size)
+            attn_output = gather_heads_scatter_seq(attn_output, head_dim=1, seq_dim=0)
+        attn_output = attn_output.reshape(hidden_states.shape[0], -1)
+        attn_output = self.proj(attn_output)
+        return attn_output
+
+
+QWEN2_5_VL_VISION_ATTENTION_CLASSES = {
+    "eager": Qwen2_5_VLVisionAttention,
+    "flash_attention_2": Qwen2_5_VLVisionSdpaAttention,
+    "sdpa": Qwen2_5_VLVisionSdpaAttention,
+}
 
 
 
-return torch_npu.npu_rms_norm(hidden_states, self.weight, epsilon=self.variance_epsilon)[0]
 
 
+class Qwen2_5_VLSdpaAttention(Qwen2_5_VLAttention):
+    """
+    Qwen2 attention module using torch.nn.functional.scaled_dot_product_attention. This module inherits from
+    `Qwen2Attention` as the weights of the module stays untouched. The only changes are on the forward pass to adapt to
+    SDPA API.
+    """
 
-    q_embed = torch_npu.npu_rotary_mul(q.unsqueeze(0), cos.unsqueeze(0), sin.unsqueeze(0)).squeeze(0)
-    k_embed = torch_npu.npu_rotary_mul(k.unsqueeze(0), cos.unsqueeze(0), sin.unsqueeze(0)).squeeze(0)
+    # Adapted from Qwen2Attention.forward
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # necessary, but kept here for BC
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if output_attentions:
+            # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
+            logger.warning_once(
+                "Qwen2_5_VLModel is using Qwen2_5_VLSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+                'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+            )
+            return super().forward(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+
+        # ulysses sp patch: qkv proj
+        if get_parallel_state().ulysses_enabled:
+            query_states = gather_seq_scatter_heads(query_states, seq_dim=2, head_dim=1)
+            key_states = gather_seq_scatter_heads(key_states, seq_dim=2, head_dim=1)
+            value_states = gather_seq_scatter_heads(value_states, seq_dim=2, head_dim=1)
+
+        cos, sin = position_embeddings
+        query_states, key_states = apply_multimodal_rotary_pos_emb(
+            query_states, key_states, cos, sin, self.rope_scaling["mrope_section"]
+        )
+
+        if past_key_value is not None:
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}  # Specific to RoPE models
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        causal_mask = attention_mask
+        if attention_mask is not None:  # no matter the length, we just slice it
+            causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and attention_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        # The q_len > 1 is necessary to match with AttentionMaskConverter.to_causal_4d that does not create a causal mask in case q_len == 1.
+        is_causal = True if causal_mask is None and q_len > 1 else False
+
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query_states,
+            key_states,
+            value_states,
+            attn_mask=causal_mask,
+            dropout_p=self.attention_dropout if self.training else 0.0,
+            is_causal=is_causal,
+        )
+
+        # ulysses sp patch: o projection
+        if get_parallel_state().ulysses_enabled:
+            attn_output = gather_heads_scatter_seq(attn_output, head_dim=2, seq_dim=1)
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+
+
+QWEN2_5_VL_ATTENTION_CLASSES = {
+    "eager": Qwen2_5_VLAttention,
+    "flash_attention_2": Qwen2_5_VLSdpaAttention,
+    "sdpa": Qwen2_5_VLSdpaAttention,
+}
